f96f28110545b317af0b3960b094e1acdefbd28a
yannickRafael <yannickrafael286@gmail.com>
2026-01-05 22:13:54 +0200

    Implement Client and Request data models

---
+++ lib/models/client.dart
@@ -0,0 +1,33 @@
+class Client {
+  final String id;
+  final String name;
+  final String phoneNumber;
+  final String address;
+
+  Client({
+    required this.id,
+    required this.name,
+    required this.phoneNumber,
+    required this.address,
+  });
+
+  // Factory to create a Client from a Map (e.g., from JSON/DB in future)
+  factory Client.fromMap(Map<String, dynamic> map) {
+    return Client(
+      id: map['id'] ?? '',
+      name: map['name'] ?? '',
+      phoneNumber: map['phoneNumber'] ?? '',
+      address: map['address'] ?? '',
+    );
+  }
+
+  // Method to convert Client to Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'phoneNumber': phoneNumber,
+      'address': address,
+    };
+  }
+}
---
+++ lib/models/request.dart
@@ -0,0 +1,54 @@
+enum ProductType {
+  chicken,
+  eggs,
+}
+
+enum PaymentStatus {
+  pending,
+  paid,
+  partial,
+}
+
+class Request {
+  final String id;
+  final String clientId;
+  final ProductType type;
+  final double amount; // quantity
+  final double totalPrice;
+  final DateTime date;
+  PaymentStatus paymentStatus;
+
+  Request({
+    required this.id,
+    required this.clientId,
+    required this.type,
+    required this.amount,
+    required this.totalPrice,
+    required this.date,
+    this.paymentStatus = PaymentStatus.pending,
+  });
+
+  factory Request.fromMap(Map<String, dynamic> map) {
+    return Request(
+      id: map['id'] ?? '',
+      clientId: map['clientId'] ?? '',
+      type: ProductType.values[map['type'] ?? 0],
+      amount: (map['amount'] ?? 0).toDouble(),
+      totalPrice: (map['totalPrice'] ?? 0).toDouble(),
+      date: DateTime.parse(map['date']),
+      paymentStatus: PaymentStatus.values[map['paymentStatus'] ?? 0],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'clientId': clientId,
+      'type': type.index,
+      'amount': amount,
+      'totalPrice': totalPrice,
+      'date': date.toIso8601String(),
+      'paymentStatus': paymentStatus.index,
+    };
+  }
+}
